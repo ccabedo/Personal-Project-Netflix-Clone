d83690af4b75088023c812423d56462ab84a6192
Colleen Cabedo <cmcabedo@gmail.com>
2023-01-10 17:21:19 -0500

    built components

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDTTbfuzuSzSyEm-DpcOPAdiYL54B7OnLc",
+    authDomain: "netflix-clone-e33bb.firebaseapp.com",
+    projectId: "netflix-clone-e33bb",
+    storageBucket: "netflix-clone-e33bb.appspot.com",
+    messagingSenderId: "845384415964",
+    appId: "1:845384415964:web:f84a88d46fb17d60f7d219"
+  };
